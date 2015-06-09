db4d467f8e0545f9fd980115008aa933c7742c77
chenjunxyf <1040411807@qq.com>
2015-06-09 15:44:02 +0800

    left cross-domain demos upload

--- cross-domain/routes/index.js
+++ cross-domain/routes/index.js
@@ -30,9 +30,52 @@ router.get('/document_domain_test', function(req, res) {
   res.render('document-domain/document_domain_test');
 });
 
-router.get('/test_iframe', function(req, res) {
-  res.render('document-domain/test_iframe');
+router.get('/document_iframe', function(req, res) {
+  res.render('document-domain/document_iframe');
 });
 
+/* url-hash Test */
+router.get('/url_hash_test', function(req, res) {
+  res.render('url-hash/url_hash_test');
+});
+
+router.get('/hash_iframe', function(req, res) {
+  res.render('url-hash/hash_iframe');
+});
+
+/* cross-fragment Test */
+router.get('/cross_fragment_test', function(req, res) {
+  res.render('cross-fragment/cross_fragment_test');
+});
+
+router.get('/fragment_iframe', function(req, res) {
+  res.render('cross-fragment/fragment_iframe');
+});
+
+router.get('/fragment_req_proxy', function(req, res) {
+  res.render('cross-fragment/fragment_req_proxy');
+});
+
+router.get('/fragment_res_proxy', function(req, res) {
+  res.render('cross-fragment/fragment_res_proxy');
+});
+
+/* window-name Test */
+router.get('/window_name_test', function(req, res) {
+  res.render('window-name/window_name_test');
+});
+
+router.get('/window_iframe', function(req, res) {
+  res.render('window-name/window_iframe');
+});
+
+/* postMessage Test */
+router.get('/postMessage_test', function(req, res) {
+  res.render('postMessage/postMessage_test');
+});
+
+router.get('/postMessage_iframe', function(req, res) {
+  res.render('postMessage/postMessage_iframe');
+});
 
 module.exports = router;
